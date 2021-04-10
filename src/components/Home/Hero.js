8aa725fc090aab4137299c381dcecc5ea4513d4d
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-11 00:23:44 +0800

    Added needed packages.

---
+++ src/components/Home/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <header className='hero'>
+    </header>
+  )
+}
+
+export default Hero
---
+++ src/components/Math/Math.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {MathComponent} from 'mathjax-react'
+
+export let M = (props) => {
+  return <MathComponent tex={props.m} display={false} />
+}
+export let Mb = (props) => {
+  return <MathComponent tex={props.m} display={true} />
+}
