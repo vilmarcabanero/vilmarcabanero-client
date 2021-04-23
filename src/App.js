b33d72ae706d6b485d6290eefa0643ff8f39d501
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-24 07:19:40 +0800

    Solved the problem with iframe width, from fixed to 100% para yung div container nalang i.adjust

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react'
 // import Header from './components/Home/Header'
 // import Hero from './components/Home/Hero'
 import './App.scss'
-import logo from './assets/images/logo.JPG'
-import styled from 'styled-components'
+import logo from './assets/images/logo.png'
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className='video'>
-				<iframe
+				<iframe title="Algebra 1.3"
 					src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
-					width='640px'
-					height='480px'
+					width='100%'
+					height='100%'
 					frameborder='0'
 					scrolling='no'
 					seamless=''
