934f8103d67af57dca6111d9d0087065b1c740df
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-24 06:52:18 +0800

    Tried to embed gdrive video and hide the pop-out and activated the full screen mode.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from 'react'
 // import Header from './components/Home/Header'
 // import Hero from './components/Home/Hero'
 import './App.scss'
+import logo from './assets/images/logo.JPG'
+import styled from 'styled-components'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        {/* <Header/>
-        <Hero/> */}
-        
-      </div> 
-    )
-  }
+	render() {
+		return (
+			<div className='video'>
+				<iframe
+					src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
+					width='640px'
+					height='480px'
+					frameborder='0'
+					scrolling='no'
+					seamless=''
+					allowfullscreen='true'
+					webkitallowfullscreen='true'
+					mozallowfullscreen='true'
+				></iframe>
+
+				<div>&nbsp;</div>
+				<img src={logo} alt='afdafd' />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
