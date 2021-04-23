b6a2f5d4af6a5e5254571b980d2571168516ae8d
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-23 22:57:54 +0800

    Back to zero. Start with apps, right part of nav would be account icon with login and apps icon, dark theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Header from './components/Home/Header'
-import Hero from './components/Home/Hero'
+// import Header from './components/Home/Header'
+// import Hero from './components/Home/Hero'
 import './App.scss'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Hero/>
+        {/* <Header/>
+        <Hero/> */}
+        
       </div> 
     )
   }
