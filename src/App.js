d17526b0a1f052b6ddfdb3abdae0d7f8deddc42f
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-25 21:53:01 +0800

    Solved the articles problem with size

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React from 'react'
 // import Header from './components/Home/Header'
 // import Hero from './components/Home/Hero'
+// import ParticleBackground from './config/ParticleBackground'
+// import Particles from 'react-particles-js'
 import './App.scss'
+import ParticleBackground from './config/ParticleBackground'
 import logo from './assets/images/logo.png'
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className='video'>
-				<iframe title="Algebra 1.3"
-					src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
-					width='100%'
-					height='100%'
-					frameborder='0'
-					scrolling='no'
-					seamless=''
-					allowfullscreen='true'
-					webkitallowfullscreen='true'
-					mozallowfullscreen='true'
-				></iframe>
-				<div>&nbsp;</div>
-				<img src={logo} alt='afdafd' />
-			</div>
+			<>
+				<ParticleBackground />
+
+				<div className='video'>
+					<iframe
+						title='Algebra 1.3'
+						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
+						width='100%'
+						height='100%'
+						frameborder='0'
+						scrolling='no'
+						seamless=''
+						allowfullscreen='true'
+						webkitallowfullscreen='true'
+						mozallowfullscreen='true'
+					></iframe>
+					<div>&nbsp;</div>
+					<img src={logo} alt='afdafd' />
+				</div>
+
+				<div className='video'>
+					<iframe
+						title='Algebra 1.3'
+						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
+						width='100%'
+						height='100%'
+						frameborder='0'
+						scrolling='no'
+						seamless=''
+						allowfullscreen='true'
+						webkitallowfullscreen='true'
+						mozallowfullscreen='true'
+					></iframe>
+					<div>&nbsp;</div>
+					<img src={logo} alt='afdafd' />
+				</div>
+
+				<div className='video'>
+					<iframe
+						title='Algebra 1.3'
+						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
+						width='100%'
+						height='100%'
+						frameborder='0'
+						scrolling='no'
+						seamless=''
+						allowfullscreen='true'
+						webkitallowfullscreen='true'
+						mozallowfullscreen='true'
+					></iframe>
+					<div>&nbsp;</div>
+					<img src={logo} alt='afdafd' />
+				</div>
+			</>
 		)
 	}
 }
---
+++ src/config/ParticleBackground.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import Particles from 'react-particles-js'
+import particleParams from './particleParams'
+
+export default function ParticleBackground() {
+	return (
+		<StyledParticles>
+			<Particles className='particles' params={particleParams}></Particles>
+		</StyledParticles>
+	)
+}
+
+const StyledParticles = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: #4398ff;
+
+	.particles {
+		width: 100%;
+	}
+`
