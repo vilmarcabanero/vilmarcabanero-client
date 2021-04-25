5e52136abdf6a82fbeed9c21a3983296f31b9621
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-25 23:56:32 +0800

    Removed videos.

--- src/App.js
+++ src/App.js
@@ -3,64 +3,13 @@ import React from 'react'
 // import Hero from './components/Home/Hero'
 import './App.scss'
 import ParticleBackground from './Particles/ParticleBackground'
-import logo from './assets/images/logo.png'
+
 
 class App extends React.Component {
 	render() {
 		return (
 			<>
 				<ParticleBackground />
-
-				<div className='video'>
-					<iframe
-						title='Algebra 1.3'
-						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
-						width='100%'
-						height='100%'
-						frameborder='0'
-						scrolling='no'
-						seamless=''
-						allowfullscreen='true'
-						webkitallowfullscreen='true'
-						mozallowfullscreen='true'
-					></iframe>
-					<div>&nbsp;</div>
-					<img src={logo} alt='afdafd' />
-				</div>
-
-				<div className='video'>
-					<iframe
-						title='Algebra 1.3'
-						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
-						width='100%'
-						height='100%'
-						frameborder='0'
-						scrolling='no'
-						seamless=''
-						allowfullscreen='true'
-						webkitallowfullscreen='true'
-						mozallowfullscreen='true'
-					></iframe>
-					<div>&nbsp;</div>
-					<img src={logo} alt='afdafd' />
-				</div>
-
-				<div className='video'>
-					<iframe
-						title='Algebra 1.3'
-						src='https://drive.google.com/file/d/1XQjeVRgjvglsDWS-c9wVxAFIdD4k6CS3/preview'
-						width='100%'
-						height='100%'
-						frameborder='0'
-						scrolling='no'
-						seamless=''
-						allowfullscreen='true'
-						webkitallowfullscreen='true'
-						mozallowfullscreen='true'
-					></iframe>
-					<div>&nbsp;</div>
-					<img src={logo} alt='afdafd' />
-				</div>
 			</>
 		)
 	}
